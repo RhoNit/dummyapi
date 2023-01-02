89d16372376c7b4896754901b8927c9b5dff16bc
Ranit Biswas <60309029+RhoNit@users.noreply.github.com>
2023-01-02 12:17:57 +0530

    🧱 DAO layer is added

--- src/main/java/com/ranit/dummyapi/dao/BookRepository.java
+++ src/main/java/com/ranit/dummyapi/dao/BookRepository.java
@@ -4,6 +4,7 @@ import com.ranit.dummyapi.models.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
     public Book findById(int id);
